0e0c585fb8c71b30d91ab798479b1d992c7f05ae
gordondalos <gordondalos@gmail.com>
2015-12-01 10:39:33 +0600

    заделал шаблон

---
+++ gordondalos_template/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include"parts/head.php"?>
+<body>
+<div class="page">
+
+    <?php include"parts/header.php"?>
+    <?php include"parts/main.php"?>
+    <?php include"parts/footer.php"?>
+
+
+</div>
+<?php include"parts/js.php"?>
+</body>
+</html>
---
+++ gordondalos_template/parts/footer.php
@@ -0,0 +1,82 @@
+<!--
+ ========================================================
+                             FOOTER
+ ========================================================
+ -->
+<footer>
+    <div class="container well">
+        <div class="row">
+            <div class="col-md-3 col-xs-6 col-sm-6">
+                <h4 class="primary-color">Get in touch</h4>
+                <address class="secondary5-color">8901 Marmora Road <br/>Glasgow, DO4 89GR.</address>
+                <ul class="contact-list">
+                    <li>
+                        <dl>
+                            <dt>Freephone:</dt>
+                            <dd><a href="callto:x">+1 800 559 6580</a></dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt>Telephone:</dt>
+                            <dd><a href="callto:x">+1 959 603 6035</a></dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt>FAX:</dt>
+                            <dd class="mod1"><a href="callto:x">+ 1 504 889 9898</a></dd>
+                        </dl>
+                    </li>
+                    <li>
+                        <dl>
+                            <dt>E-mail:</dt>
+                            <dd><a href="mailto:x">mail@demolink.org</a></dd>
+                        </dl>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-2 col-xs-6 col-sm-6">
+                <h4 class="primary-color">Menu</h4>
+                <ul class="nav-list">
+                    <li><a href="./">Home</a></li>
+                    <li><a href="index-1.html">About us</a></li>
+                    <li><a href="index-2.html">How to order</a></li>
+                    <li><a href="index-3.html">FAQ</a></li>
+                    <li><a href="index-4.html">Jobs</a></li>
+                    <li><a href="index-5.html">Contacts</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-xs-6 col-sm-6">
+                <h4 class="primary-color">Follow us</h4>
+                <ul class="inline-list">
+                    <li><a href="#" class="fa fa-google-plus"></a></li>
+                    <li><a href="#" class="fa fa-twitter"></a></li>
+                    <li><a href="#" class="fa fa-facebook"></a></li>
+                    <li><a href="#" class="fa fa-pinterest"></a></li>
+                    <li><a href="#" class="fa fa-linkedin"></a></li>
+                </ul>
+            </div>
+            <div class="col-md-offset-1 col-md-3 col-xs-6 col-sm-6">
+                <h4 class="primary-color">Join our newsletter</h4>
+
+                <p>And always stay informed of the latest company news and events!</p><a href="#" class="btn">Sign
+                    up</a>
+            </div>
+        </div>
+    </div>
+    <div class="banner">
+        <div class="container">
+            <p><em>© <span id="copyright-year"></span> Sancho.</em> Food Delivery. All rights reserved. &nbsp;</p><a
+                href="index-6.html">Privacy policy</a><!-- {%FOOTER_LINK} -->
+        </div>
+    </div>
+    <div class="map">
+        <div id="google-map" class="map_model"></div>
+        <ul class="map_locations">
+            <li data-x="-73.9874068" data-y="40.643180">
+                <p>9870 St Vincent Place, Glasgow, DC 45 Fr 45. <span>800 2345-6789</span></p>
+            </li>
+        </ul>
+    </div>
+</footer>
